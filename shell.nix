ff194f0983808ab64cd763887de8e9323b552e59
Thomas Tuegel <ttuegel@mailbox.org>
2021-03-16 16:02:08 -0500

    Remove last Nix artifacts

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources."nixpkgs" { overlays = []; config = {}; };
-  inherit (pkgs) mkShell nix-prefetch-scripts jq;
-in
-
-mkShell {
-  buildInputs = [ nix-prefetch-scripts jq ];
-}
